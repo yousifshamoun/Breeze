d3ebf7695f76943e68a7214f6c418ad0a6223151
yshamoun <yousif2002@berkeley.edu>
2023-05-13 18:25:11 -0700

    Initial Commit

---
+++ Breeze/BreezeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreezeApp.swift
+//  Breeze
+//
+//  Created by Yousif  Shamoun  on 5/13/23.
+//
+
+import SwiftUI
+
+@main
+struct BreezeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
